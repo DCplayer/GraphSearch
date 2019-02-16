cc6c614a2c305bc52541eed6df8ef67fd50fadee
CoffeeBrewer <cas15151@uvg.edu.gt>
2019-02-16 03:27:45 -0600

    Creating class sudoku for solving a  4x4 sudoku. Determining the real function of actions() and result() and how to use it

---
+++ sudoku.py
@@ -0,0 +1,16 @@
+
+class sudoku:
+    def __init__(self, sudoku_string):
+
+        self.sudoku_string = sudoku_string
+        self.change = False
+        self.probability = {}
+        self.actions = []
+
+def actions(string_sudoku):
+    string_estado = list(string_sudoku)
+
+    return
+
+def result(string_estado, indice, cambio):
+    return
