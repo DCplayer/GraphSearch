71a832840bf794663834ded724c4322467779a96
CoffeeBrewer <cas15151@uvg.edu.gt>
2019-02-21 04:36:04 -0600

    Faltan tres funciones. Funcion de backtrack con self.actual_string, funcion de heuristica para saber cuanto pesa el estado y funcion para posicionar un state basado en su peso de heuristica. Despues de eso es debuggear y pruebas :D

--- fifteen.py
+++ fifteen.py
@@ -30,21 +30,12 @@ class Fifteen:
             '.': 16
         }
         self.record = []
-        self.queue_list = []
-        self.state_cost = []
         self.backtrack = {
             self.initial_string: 'END'
         }
+        self.state_cost = []
         self.explored = []
         self.frontiers = []
-
-    def organize_hex_list(self):
-        for i in range(16):
-            if self.fifteen_string[i] == '.':
-                self.fifteen_string[i] = 16
-            else:
-                self.fifteen_string[i] = int(self.fifteen_string[i], 16)
-        print(self.fifteen_string)
 
     def solvable_puzzle(self):
         inversions = 0
@@ -77,6 +68,7 @@ class Fifteen:
 
     def goal_test(self):
         if self.fifteen_string == ['1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F', '.']:
+            self.finish = True
             return True
         else:
             return False
@@ -92,11 +84,19 @@ class Fifteen:
             action_list.append('L')
         if index % 4 < 3:
             action_list.append('R')
-        return
+        return action_list
 
     def result(self, state, action):
-        if action == 'UP':
-            return self.move_up()
+        if action == 'U':
+            return self.move_up(state)
+        elif action == 'D':
+            return self.move_down(state)
+        elif action == 'L':
+            return self.move_left(state)
+        elif action == 'R':
+            return self.move_right(state)
+        else:
+            print("Something went terrible wrong with: result(self, state, action)")
         return
 
     def move_up(self, state):
@@ -104,28 +104,40 @@ class Fifteen:
         base = list(state)
         base[index] = base[index - 4]
         base[index - 4] = '.'
-        return ''.join(base)
+
+        string_result = ''.join(base)
+        self.backtrack[string_result] = state
+        return string_result
 
     def move_down(self, state):
         index = self.fifteen_string.index('.')
         base = list(state)
         base[index] = base[index + 4]
         base[index + 4] = '.'
-        return ''.join(base)
+
+        string_result = ''.join(base)
+        self.backtrack[string_result] = state
+        return string_result
 
     def move_left(self, state):
         index = self.fifteen_string.index('.')
         base = list(state)
         base[index] = base[index - 1]
         base[index - 1] = '.'
-        return ''.join(base)
+
+        string_result = ''.join(base)
+        self.backtrack[string_result] = state
+        return string_result
 
     def move_right(self, state):
         index = self.fifteen_string.index('.')
         base = list(state)
         base[index] = base[index + 1]
         base[index + 1] = '.'
-        return ''.join(base)
+
+        string_result = ''.join(base)
+        self.backtrack[string_result] = state
+        return string_result
 
     def main(self):
         if not self.solvable_puzzle():
@@ -135,6 +147,23 @@ class Fifteen:
                   "mas completa")
             return
 
+        self.frontiers.append(''.join(self.fifteen_string))
+        self.state_cost.append(1000000000000000)
+        while not self.finish and self.change:
+            self.actual_string = self.frontiers[0]
+            if self.goal_test(self.actual_string):
+                #Funcion de backtrack con self.actual_string
+                break
+            else:
+                self.frontiers.remove(0)
+                self.state_cost.remove(0)
+                self.explored.append(self.actual_string)
+                action_list = self.actions(self.actual_string)
+                for i in action_list:
+                    next_state = self.result(self.actual_string, i)
+                    self.frontiers.append(next_state)
+                    #funcion de heuristica para saber cuanto pesa el next_state
+                    #Funcion para posicionar un next_state basado en su peso de heuristica
 
 fifteen = Fifteen('D2A31C845.96FEB7')
 print(fifteen.solvable_puzzle())
