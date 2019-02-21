59f415df8e4537f51bfc4d53db29a2466e04cd7d
CoffeeBrewer <cas15151@uvg.edu.gt>
2019-02-21 03:18:13 -0600

    Agregando soluciones y como detectar eficientemente si no tiene. Ademas de funciones actions y result para el algoritmo. Finalmente agregando las funciones de moverse en las 4 direcciones

--- fifteen.py
+++ fifteen.py
@@ -1,4 +1,4 @@
-
+import math
 class Fifteen:
     def __init__(self, fifteen_string):
 
@@ -9,6 +9,7 @@ class Fifteen:
         self.solvable = True
         self.finish = False
         self.inversiones = 0
+        self.row_number = 0
         self.sectors = {
             0: hex(0),
             1: hex(1),
@@ -58,6 +59,53 @@ class Fifteen:
         self.inversiones = inversions
         self.fifteen_string.insert(index_space, 16)
 
+        self.row_number = 4 - math.floor(index_space / 4)
+        result1 = 'ODD'
+        result2 = 'ODD'
+
+        if inversions % 2 == 0:
+            result1 = 'EVEN'
+
+        if self.row_number % 2 == 0:
+            result2 = 'EVEN'
+
+        if result1 != result2:
+            return True
+        else:
+            return False
+
+    def goal_test(self):
+        if self.fifteen_string == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]:
+            return True
+        else:
+            return False
+
+    def actions(self):
+        return
+
+    def result(self, state, action):
+        return
+
+    def move_up(self, state):
+        return
+
+    def move_down(self, state):
+        return
+
+    def move_left(self, state):
+        return
+
+    def move_right(self, state):
+        return
+
+    def main(self):
+        if not self.solvable_puzzle():
+            print("El puzzle no posee solucion. \nNúmero de inversiones: "
+                  + str(self.inversiones) + "\nPosicion de X desde el fondo: " + str(self.row_number)  +
+                  "\nVease: https://www.geeksforgeeks.org/check-instance-15-puzzle-solvable/ para una explicación"
+                  "mas completa")
+            return
+
 
 fifteen = Fifteen('D2A31C845.96FEB7')
-fifteen.solvable_puzzle()
+
